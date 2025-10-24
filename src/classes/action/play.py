46500629fbfd9a19f512bb69a3e4876b813582bf
bridge <wangxin4qiao@163.com>
2025-10-25 00:14:06 +0800

    update

--- src/classes/action/play.py
+++ src/classes/action/play.py
@@ -6,10 +6,10 @@ from src.classes.event import Event
 
 class Play(TimedAction):
     """
-    游戏娱乐动作，持续半年时间
+    消遣动作，持续半年时间
     """
 
-    COMMENT = "游戏娱乐，放松身心"
+    COMMENT = "消遣，放松身心"
     DOABLES_REQUIREMENTS = "任何时候都可以执行"
     PARAMS = {}
 
@@ -17,9 +17,9 @@ class Play(TimedAction):
 
     def _execute(self) -> None:
         """
-        进行游戏娱乐活动
+        进行消遣活动
         """
-        # 游戏娱乐的具体逻辑可以在这里实现
+        # 消遣的具体逻辑可以在这里实现
         # 比如增加心情值、减少压力等
         pass
 
@@ -27,7 +27,7 @@ class Play(TimedAction):
         return True, ""
 
     def start(self) -> Event:
-        return Event(self.world.month_stamp, f"{self.avatar.name} 开始玩耍")
+        return Event(self.world.month_stamp, f"{self.avatar.name} 开始消遣")
 
     # TimedAction 已统一 step 逻辑
 
