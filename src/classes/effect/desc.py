13c142ad71c076998f85ffc5413c4c4943ea60c1
bridge <wangxin4qiao@163.com>
2026-02-17 14:50:35 +0800

    update

--- src/classes/effect/desc.py
+++ src/classes/effect/desc.py
@@ -41,6 +41,8 @@ def get_effect_desc(effect_key: str) -> str:
         "extra_hidden_domain_drop_prob": "effect_extra_hidden_domain_drop_prob",
         "extra_hidden_domain_danger_prob": "effect_extra_hidden_domain_danger_prob",
         "extra_epiphany_probability": "effect_extra_epiphany_probability",
+        "extra_educate_efficiency": "effect_extra_educate_efficiency",
+        "extra_educate_prosperity_prob": "effect_extra_educate_prosperity_prob",
     }
     
     msgid = msgid_map.get(effect_key, effect_key)
